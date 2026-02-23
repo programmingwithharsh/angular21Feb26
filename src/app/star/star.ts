ea9b55277b4788a30713cf5d763b0e0c5053c916
Harsh Tiwari <harshtiwari343@email.com>
2026-02-23 17:09:03 +0530

    Created components

---
+++ src/app/star/star.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  imports: [],
+  templateUrl: './star.html',
+  styleUrl: './star.scss',
+})
+export class Star {
+
+}
