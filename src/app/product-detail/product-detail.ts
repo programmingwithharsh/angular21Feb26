8822128b2fdc77ebc2928439649a1545586798d9
Harsh Tiwari <harshtiwari343@email.com>
2026-02-25 13:04:51 +0530

    get params and navigation

--- src/app/product-detail/product-detail.ts
+++ src/app/product-detail/product-detail.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-product-detail',
   imports: [],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.scss',
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {
 
+  }
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    console.log(id);
+  }
+
+  goBack() {
+    this.router.navigate(["/products"]);
+  }
 }
