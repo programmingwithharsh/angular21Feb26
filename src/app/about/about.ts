b4cfd07ee1aed0b5cb4518592ba8f171b1ae1961
Harsh Tiwari <harshtiwari343@email.com>
2026-02-26 17:25:18 +0530

    Added Signals

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, OnChanges, OnDestroy, OnInit, signal, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,8 +6,42 @@ import { Component, computed, signal } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {
+export class About implements OnInit, OnChanges, OnDestroy {
   count = signal(0); // intial value is 0
+  firstname = signal('Prudhvi');
+  lastname = signal('Pachipulusu');
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  /*
+    When either firsName or lastName changes -> fullName auto recalculates
+  */
+  // computed signal
+  fullName = computed(() => {
+    return this.firstname() + ' ' + this.lastname()
+  })
+
+  changeFirst() {
+    this.firstname.set('Programming with');
+  }
+
+  changeLast() {
+    this.lastname.set('Harsh');
+  }
 
   increase() {
     this.count.update(c => c + 1);
